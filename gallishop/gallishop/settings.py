13a8268df174c420a10f8e0d1c622a6e9a6a54c3
Mubeen Khan Muhammad <mubeen@air.local>
2023-01-22 08:32:23 +0530

    Django based gallishop app with multi-container support, gallishop and mongo container

--- gallishop/gallishop/settings.py
+++ gallishop/gallishop/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'gallishop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.shopping',
+        'ENGINE': 'djongo',
+        'NAME': 'shopping',
+        'CLIENT': {
+            'host': 'mongodb://mongodb:27017',
+            'username': 'root',
+            'password': 'mongoadmin',
+            'authSource': 'admin',
+            'authMechanism': 'SCRAM-SHA-1',
+        }
     }
 }
 
